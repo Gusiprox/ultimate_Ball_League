10deaa4a2057de289d9ed61e6b0e5369832d70ec
ErikDeLaCruz <gusiprox_079_patata@hotmail.com>
2026-04-20 22:21:10 +0200

    provisional

---
+++ characters/characterDataModel.gd
@@ -0,0 +1,11 @@
+extends RefCounted
+class_name CharacterDataModel
+
+var name: String
+
+var knokForce: float
+var knokResistance: float
+var speed: float
+
+var ability: String
+var talent: String
