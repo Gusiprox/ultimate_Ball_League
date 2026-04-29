182c9f1d925bd9678d816c23f83e570a3da4a7ed
SaraPF3 <sarapefe5@gmail.com>
2026-04-29 13:08:11 +0200

    Añadidos botones a menuUser y creado menuExit

--- menus/nav_bar/nav_bar.gd
+++ menus/nav_bar/nav_bar.gd
@@ -1,12 +1,14 @@
 extends Panel
 
 @onready var menuUser = $MenuUser
+@onready var menuExit = $MenuExit
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	menuUser.visible = false
+	menuExit.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,3 +18,15 @@ func _process(delta: float) -> void:
 
 func _on_btn_menu_user_pressed() -> void:
 	menuUser.visible = !menuUser.visible
+
+
+func _on_btn_exit_pressed() -> void:
+	menuExit.visible = true
+
+
+func _on_btn_conf_exit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_btn_cancel_exit_pressed() -> void:
+	menuExit.visible = false
