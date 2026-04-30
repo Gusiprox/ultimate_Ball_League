1b7a0db5da2fa4ee166b6f336d0d6bc3dbcd0a6b
SaraPF3 <sarapefe5@gmail.com>
2026-04-30 11:50:31 +0200

    Añadido apartado de personaje a menuEditCharacter

---
+++ menus/menu_edit_character/menu_edit_character.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var menuListChar = $MenuListChar
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	menuListChar.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_btn_change_char_pressed() -> void:
+	menuListChar.visible = true
+
+
+func _on_btn_close_menu_pressed() -> void:
+	menuListChar.visible = false
