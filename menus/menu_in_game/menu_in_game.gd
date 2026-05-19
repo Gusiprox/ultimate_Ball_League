b4d64f067ca3512accb6c74b230ca7f96b512947
SaraPF3 <sarapefe5@gmail.com>
2026-05-19 13:51:44 +0200

    Cambios en script menuInGame

--- menus/menu_in_game/menu_in_game.gd
+++ menus/menu_in_game/menu_in_game.gd
@@ -5,9 +5,13 @@ extends Control
 @onready var menuOptions = $MenuOptions
 @onready var menuControls = $MenuOptions/MenuControls
 @onready var menuSound = $MenuOptions/MenuSound
+@onready var btnSound = $MenuOptions/marginMenuOpt/contMenuOptBtn/btnSonido
+@onready var btnControls = $MenuOptions/marginMenuOpt/contMenuOptBtn/btnControls
 @onready var menuExit = $MenuExit
 
 var posicionesSubmenus = {}
+var colorActivo = Color(1, 1, 1, 1)
+var colorInactivo = Color(0.577, 0.577, 0.577, 1.0)
 
 func _ready() -> void:
 	backgroundBlur.visible = true
@@ -44,12 +48,10 @@ func _on_btn_close_options_pressed() -> void:
 	animarSubmenu(panelMenuInGame)
 
 func _on_btn_sonido_pressed() -> void:
-	menuSound.visible = true
-	menuControls.visible = false
+	actualizarVisualOpciones(btnSound, menuSound)
 
 func _on_btn_controls_pressed() -> void:
-	menuSound.visible = false
-	menuControls.visible = true
+	actualizarVisualOpciones(btnControls, menuControls)
 
 
 #Animación de los submenús de MenuInGame
@@ -76,5 +78,17 @@ func animarSubmenu(menuObjetivo: Control):
 	var tween = create_tween().set_parallel(true)
 	tween.set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 	
-	tween.tween_property(menuObjetivo, "modulate:a", 1.0, 0.2)
-	tween.tween_property(menuObjetivo, "global_position:y", posFinal.y, 0.2)
+	tween.tween_property(menuObjetivo, "modulate:a", 1.0, 0.15)
+	tween.tween_property(menuObjetivo, "global_position:y", posFinal.y, 0.15)
+
+
+#Cambio de botón marcado en menuOptions
+
+func actualizarVisualOpciones(botonActivo: Button, paginaActiva: Control):
+	btnSound.modulate = colorInactivo
+	btnControls.modulate = colorInactivo
+	
+	botonActivo.modulate = colorActivo
+	menuSound.visible = false
+	menuControls.visible = false
+	paginaActiva.visible = true
