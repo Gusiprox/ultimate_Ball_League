f3584809b1f37ebde8500a18a46cc7a55728577a
SaraPF3 <sarapefe5@gmail.com>
2026-06-08 20:07:08 +0200

    Mejorado estilo createUser y gestionado errores

--- menus/login/create_user.gd
+++ menus/login/create_user.gd
@@ -1,19 +1,41 @@
 extends Control
 
-@onready var email = $email
-@onready var password = $password
-@onready var username = $username
+const USERNAME_LENGTH: int = 3
+const PASSWORD_LENGTH: int = 6
+const ARROBA: String = "@"
+
+@onready var email = $contCreateUserElem/email
+@onready var password = $contCreateUserElem/password
+@onready var username = $contCreateUserElem/username
+@onready var contError = $contCreateUserElem/contError
+@onready var contCorrect = $contCreateUserElem/contCorrect
 
 var pathMenuAll = "res://menus/menu_all/menu_all.tscn"
 
 func _ready() -> void:
+	contError.hide()
+	contCorrect.hide()
 	PlayFabManager.client.registered.connect(_onSuccessfull)
 	PlayFabManager.client.api_error.connect(_onError)
 
 func _onButtonDown() -> void:
+	contCorrect.hide()
 	var emailText: String = email.text
 	var passwordText: String = password.text
 	var usernameText: String = username.text
+	
+	if usernameText.length() < USERNAME_LENGTH:
+		contError.show()
+		return 
+		
+	if passwordText.length() < PASSWORD_LENGTH:
+		contError.show()
+		return
+		
+	if emailText.is_empty() or not ARROBA in emailText:
+		contError.show()
+		return
+		
 	var info_params = GetPlayerCombinedInfoRequestParams.new()
 	
 	info_params.GetUserVirtualCurrency = true
@@ -28,8 +50,11 @@ func _onButtonDown() -> void:
 	)
 
 func _onSuccessfull(data) -> void:
+	contError.hide()
+	contCorrect.show()
 	pass
 	# Crear aqui algo para salir del menu de crear usuario y decir que ha salido bien
 	
 func _onError(result) -> void:
+	contError.show()
 	print("Fallo") # Aqui los fallos
